650ef25ca95c58062218900ae7a70d8f93234551
Joshna Reddy <joshnareddy202@gmail.com>
2026-03-29 01:30:38 +0530

    Integrated real OrangeHRM demo for automation

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,19 +1,20 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(self.driver, 10)
+        # Real Selectors for OrangeHRM
+        self.username_input = (By.NAME, "username")
+        self.password_input = (By.NAME, "password")
+        self.login_button = (By.CSS_SELECTOR, "button[type='submit']")
 
-        # REAL locators from SauceDemo
-        self.username_input = (By.ID, "user-name")
-        self.password_input = (By.ID, "password")
-        self.login_button = (By.ID, "login-button")
-
-    def enter_username(self, username):
-        self.driver.find_element(*self.username_input).send_keys(username)
-
-    def enter_password(self, password):
-        self.driver.find_element(*self.password_input).send_keys(password)
+    def enter_credentials(self, user, pwd):
+        # Wait for elements to appear (Standard Professional Practice)
+        self.wait.until(EC.presence_of_element_id(self.username_input)).send_keys(user)
+        self.driver.find_element(*self.password_input).send_keys(pwd)
 
     def click_login(self):
         self.driver.find_element(*self.login_button).click()
